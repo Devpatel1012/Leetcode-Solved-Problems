328c6305576bc03603dec85408e2a2e0631465d0
Devpatel1012 <devp63081@gmail.com>
2025-08-29 06:31:40 +0530

    Today's leetcode problem is solved date : '29-08-2025'

---
+++ Pascal's Triangle.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        my_list = [[0]*i for i in range(1, numRows+1)]
+        
+        for i in range(len(my_list)):
+            n = len(my_list[i])
+            for j in range(n):
+                if j == 0 or j == n-1:
+                    my_list[i][j] = 1
+                else:
+                    my_list[i][j] = my_list[i-1][j-1]+my_list[i-1][j]
+
+        return my_list
