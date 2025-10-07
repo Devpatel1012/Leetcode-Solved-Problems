e57535516c1c9292ce5013a31df1013ea5d0409d
Devpatel1012 <devp63081@gmail.com>
2025-10-07 08:12:19 +0530

    Today's leetcode problem is solved date : '7-10-2025'

---
+++ Excel Sheet Column Number.py
@@ -0,0 +1,14 @@
+
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        result = 0
+        for char in columnTitle:
+            # Convert letter to number (A=1 ... Z=26)
+            value = ord(char) - ord('A') + 1
+            # Base-26 accumulation
+            result = result * 26 + value
+        return result
