24775a3f9e6442e6131fd921dcaf00060188acb4
Devpatel1012 <devp63081@gmail.com>
2026-05-18 16:47:58 +0530

    Today's leetcode problem is solved date : '18-05-2026'

---
+++ Jump Game IV.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def minJumps(self, arr):
+        n = len(arr)
+
+        if n <= 1:
+            return 0
+
+        graph = {}
+
+        for i in xrange(n):
+            if arr[i] in graph:
+                graph[arr[i]].append(i)
+            else:
+                graph[arr[i]] = [i]
+
+        curs = [0]
+        visited = set([0])
+        step = 0
+
+        while curs:
+            nex = []
+
+            for node in curs:
+
+                if node == n - 1:
+                    return step
+
+                for child in graph[arr[node]]:
+                    if child not in visited:
+                        visited.add(child)
+                        nex.append(child)
+
+                graph[arr[node]] = []
+
+                for child in [node - 1, node + 1]:
+                    if 0 <= child < n and child not in visited:
+                        visited.add(child)
+                        nex.append(child)
+
+            curs = nex
+            step += 1
+
+        return -1
