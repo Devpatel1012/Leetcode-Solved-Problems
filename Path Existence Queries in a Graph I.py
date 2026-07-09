e9a3b1a77ed424aed919687c6a92dc2ec8affe0c
Devpatel1012 <devp63081@gmail.com>
2026-07-09 06:49:52 +0530

    Today's leetcode problem is solved date : '09-07-2026'

---
+++ Path Existence Queries in a Graph I.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def pathExistenceQueries(self, n, nums, maxDiff, queries):
+
+        group = [0] * n
+        currentGroup = 0
+        
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] > maxDiff:
+                currentGroup += 1
+            group[i] = currentGroup
+        
+        res = []
+        for u, v in queries:
+            res.append(group[u] == group[v])
+        
+        return res
