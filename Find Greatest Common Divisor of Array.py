6acad1d98cffe05a0de65a348826a059db5f79e3
Devpatel1012 <devp63081@gmail.com>
2026-07-18 06:32:52 +0530

    Today's leetcode problem is solved date : '18-07-2026'

---
+++ Find Greatest Common Divisor of Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findGCD(self, nums):
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+        mi = min(nums)
+        ma = max(nums)
+        return gcd(mi,ma)
+        
