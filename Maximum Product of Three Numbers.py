5dccb92c878d1844f9cc778eefcfe81dd9651d69
Devpatel1012 <devp63081@gmail.com>
2026-07-26 07:29:52 +0530

    Today's leetcode problem is solved date : '26-07-2026'

---
+++ Maximum Product of Three Numbers.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maximumProduct(self, A):
+        A.sort()
+        
+        return max(
+            A[-1] * A[-2] * A[-3],
+            A[-1] * A[0] * A[1]
+        )
