fb8f7b9b0a57df58913b89ed40789d2eb140649f
Devpatel1012 <devp63081@gmail.com>
2026-07-30 07:08:32 +0530

    Today's leetcode problem is solved date : '30-07-2026'

---
+++ Smallest Palindromic Rearrangement I.py
@@ -0,0 +1,13 @@
+
+class Solution(object):
+    def smallestPalindrome(self, s):
+        store = defaultdict(int)
+        res = ''
+        singles = ''
+        for i in s:
+            store[i]+=1
+        for letter in string.ascii_lowercase:
+            res+=letter*(store[letter]//2)
+            if store[letter]%2==1:
+                singles+=letter
+        return res+singles+res[::-1]
